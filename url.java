e55f39c4244c516163233f4eabc359f5133174cf
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-07 08:06:41 -0700

    Full meme data including links and first analysis

---
+++ url.java
@@ -0,0 +1,72 @@
+public class url{
+  public static void main(String[] args) {
+
+    // PHOTO_NAME = [URL, Author, "TITLE", Name of Website, Year]
+
+
+    String Roast1 = ["https://www.memedroid.com/memes/tag/roast+me", "DerpGod", "Roast Me! Let me hear something new!", "Memedroid", "2019-05-19"];
+    String Roast2 = ["https://ruinmyweek.com/funny/best-worst-reddits-roast-me/", "Max", "ruinmyweek", "ruinmyweek.com", null];
+    String Roast3 = ["https://www.reddit.com/r/RoastMe/comments/7wnads/roast_me_to_meme_hell/", "Max", "ruinmyweek", "2018-02-10"];
+    String Roast4 = ["https://www.pinterest.com/pin/429319776959933946/?nic=1a", "Gregory Jillian Sepeda", "YoungRoast", "pinterest", "2015-11-10"];
+    String Roast5 = ["https://imgur.com/gallery/NkBKd", "youandmeandrainbows", "DirtbikeGirl", "Imgur", "2017-02-13"];
+    String Roast6 = ["https://www.reddit.com/r/RoastMe/comments/5eeh3w/i_run_a_meme_page_roast_me/", "poptart999", "ChildGraphPaper", "reddit", "2016-11-22"];
+    String Roast7 = ["https://ruinmyweek.com/funny/best-worst-reddits-roast-me/", "Max", "DrawFace", "ruinmyweek", null];
+    String Roast8 = ["https://imgur.com/gallery/5x0qf", "kimthon", "KermitTheFrog", "imgur", "2015-11-05"];
+    String Roast9 = ["https://memebase.cheezburger.com/tag/roast-me", "Memebase", "Child", "Memebase", null];
+    String Roast10 = ["https://www.pinterest.com/pin/329044316515010634", "RedaAzzi","ChihuahuaMan", "pinterest", null];
+    String Roast11 = ["https://runt-of-the-web.com/savage-reddit-roasts", null, "FemaleQuasimo", "reddit", null];
+    String Roast12 = ["http://canyouactually.com/roast-me/", "reddit", "Duck/Hippo Face", "canyouactually" "2019-03-18"];
+    String Roast13 = ["https://ruinmyweek.com/funny/best-worst-reddits-roast-me/", "Max", "DobbyChild", "ruinmyweek", null];
+    String Roast14 = ["https://aminoapps.com/c/dank/page/blog/roast-me-memes/pYgL_MlFQunBRE5rEwZR7lZgd4V86LnoLl", "Sugihara", "fontIsHotter", "aminoapps", "2017-01-01"];
+    String Roast15 = ["https://imgur.com/gallery/uKVS0", "ThumYorky", "Unenthusiastic", "imgur", "2015-08-16"];
+    String Roast16 = ["https://memes.com/people-ask-the-internet-to-roast-them-the-internet-brutally-delivers", null, "AuthorCoffeeMan", "memes", "2017"];
+    String Roast17 = ["https://me.me/i/r-roast-me-d-h-lazarus-pits-7-points-13-607652", null, "SledgeHead", "me.me", null];
+    String Roast18 = ["https://www.pinterest.com/pin/510666045233531230/", "eBaumsWorld", "Bobblehead", "pinterest", null];
+    String Roast19 = ["https://tastelessgentlemen.com/roastme-ttg-edition-no5/", "Jake", "HarryIceCube", "tastelessgentlemen", "2017-04-03"];
+    String Roast20 = ["https://www.indy100.com/article/instagram-star-roasted-reddit-deleted-her-account-trolled-roast-me-niece-waidhofer-7736416", "Nieve Waidhoifer", "PhotoshopGirl", "indy100", "2017-05-15"];
+    String Roast21 = ["https://imgur.com/gallery/KMcgi", "calvincubus", "toothGap", "imgur", "2017-07-17"];
+    String Roast22 = [null, null, null, null, null];
+    String Roast23 = ["https://ruinmyweek.com/funny/best-worst-reddits-roast-me/", "Max", "gopherKid", "ruinmyweek", null];
+    String Roast24 = ["https://runt-of-the-web.com/savage-reddit-roasts", null, "administrativeAssistant", "reddit", null];
+    String Roast25 = ["https://imgur.com/a/SHzu7/layout/grid", null, null, "imgur", null];
+    String Roast26 = ["https://www.reddit.com/r/RoastMe/comments/3irl1m/just_do_it/", "Skquad", "tryTooHard", "reddit", "2016-10-10"];
+    String Roast27 = ["https://www.memedroid.com/memes/tag/roast+me", "kinsiyo", "sadParents", "memedroid", "2015-12-08"];
+    String Roast28 = ["https://www.ebaumsworld.com/pictures/the-best-of-roast-me/84711273/", "eBaumsWorld", "cancerGirl", "eBaumsWorld", "2015-08-26"];
+    String Roast29 = ["https://funnyjunk.com/Best+of+roast+me/funny-pictures/5830000/", "fennsettle", "hockeyDude", "funnyjunk", null];
+    String Roast30 = ["https://knowyourmeme.com/photos/1567441-reddit", "bridget", "floridaMan", "knowyourmeme", "2019-09"];
+    String Roast31 = ["https://memecollection.net/roast-me/", "ADMIN", "homeworkGirl", "memecollection", "2019-03-23"];
+    String Roast32 = ["https://imgur.com/gallery/32Q3C", "slipnsliding", "uOfADude", "imgur", "2016-01-23"];
+    String Roast33 = ["https://www.pinterest.com/pin/193514115222962519", "Laughtard", "depressedCarGirl", "pinterest", null];
+    String Roast34 = ["https://runt-of-the-web.com/savage-reddit-roasts", "runt", "farEyes", "RUNT", null];
+    String Roast35 = ["https://imgur.com/gallery/PQw9c", "JPizzlesaurus", "lightBlueShirt", "imgur", "2018"];
+    String Roast36 = ["https://www.hahahumor.com/best-of-roast-me/", "Waseem", "FlordiaState", "reddit", "2017-10-11"];
+    String Roast37 = ["https://breakbrunch.com/11-epic-roast-me-from-reddit/", null, "brightGreenShirt", "breakbrunch", null];
+    String Roast38 = ["https://imgur.com/gallery/E9ZGI", "TheUsernameThatExists", "schoolOffender", "imgur", "2017-01-25"];
+    String Roast39 = ["https://www.reddit.com/user/jeezone?sort=hot", "jeezone", "rabbitGirl", "reddit", "2018"];
+    String Roast40 = ["https://imgur.com/a/U7i2O/layout/grid", null, "facePainting", "imgur", "2016-07-08"];
+    String Roast41 = ["https://imgur.com/a/iapWj/layout/grid", null, "homisexualRoast", "imgur", "2016-05-28"];
+    String Roast42 = ["https://www.reddit.com/r/RedditInReddit/comments/8zuf78/im_not_sure_where_to_post_this_but_im_almost/", "bigpapi46", "libraryGirl", "reddit", "2018-07-18"];
+    String Roast43 = ["https://imgur.com/a/nlaS4/layout/grid", null, "dummy", "imgur", "2016-08-22"];
+    String Roast44 = ["https://www.reddit.com/r/RoastMe/comments/5hk2zx/im_a_lesbian_roast_me/", "TheLesbianThespian", "lesbianGirl", "reddit", "2016-12-10"];
+    String Roast45 = ["https://www.taringa.net/+imagenes/imagenes-divertidas-para-pasar-la-tarde_sl5gu", "33_SonMejores", "blowUpDoll", "Imgur", "2016-11-05"];
+    String Roast46 = ["https://imgur.com/gallery/PQw9c", "JPizzlesaurus", "cornDog", "imgur", "2018-01-10"];
+    String Roast47 = [null, null, null, null, null];
+
+
+    
+
+    int total = 47;
+
+    int reddit_amt = 14;
+    int ruinmyweek_amt = 4;
+    int pinterest_amt = 4;
+    int memedroid_amt = 2;
+
+    int web_total = (reddit_amt + ruinmyweek_amt + pinterest_amt + memedroid_amt);
+
+    System.out.println(web_total);
+
+
+
+  }
+}
